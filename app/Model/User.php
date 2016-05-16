3261db06e9899fe7d36850d2801899f7a28dc862
restu <restu.suhendar@gmail.com>
2016-05-17 01:58:45 +0700

    add example model

---
+++ app/Model/User.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Model;
+
+use JayaCode\Framework\Core\Database\Model\Model;
+
+/**
+ * Class User
+ * @package App\Model
+ */
+class User extends Model
+{
+    public static $table = "users";
+}
