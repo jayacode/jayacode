6a4acacbf329239520064df52cba14fcfbbb804d
restu <restu.suhendar@gmail.com>
2016-05-17 01:58:04 +0700

    fix code style

--- public/index.php
+++ public/index.php
@@ -1,20 +1,11 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+use JayaCode\Framework\Core\Application\Application;
+
 require_once(__DIR__.'/../vendor/autoload.php');
 
+$app = new Application();
 
-/**
- * Load all routes
- */
-$routes = require_once("/../config/routes.php");
-
-/**
- * define app dir
- */
-define("__APP_DIR__", dirname(__FILE__). "/..");
-
-$app = \JayaCode\Framework\Core\Application\Application::create();
-$app->setListRoute($routes);
+$app->initConfigDir(dirname(__FILE__). "/..");
+$app->setListRoute(config("routes"));
 
 $app->run();
